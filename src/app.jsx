45855283fba498e5481b4503056ba644781952a8
Anthony Littlejohn <ant05man@yahoo.com>
2024-01-13 18:26:36 -0500

    deleted the content on the index.html

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import Homepage from "./components/homepage";
 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './app.css'; // CSS file
+
+
 function App() {
     return (
         <Router>
