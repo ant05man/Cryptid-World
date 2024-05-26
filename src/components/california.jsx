ba20c8a62ee21c84d717ecb7aff868d8314fefe7
Anthony Littlejohn <ant05man@yahoo.com>
2024-05-26 15:43:49 -0400

    created navbar for california page

--- src/components/california.jsx
+++ src/components/california.jsx
@@ -1,11 +1,19 @@
 // California.jsx
+// the style of this page is from hompage.jsx because that takes priority
 import React from 'react';
+import '../styles/california.css';
 
 function California() {
     return (
         <div>
-            <h1>Welcome to California</h1>
-            <p>This is the California page content.</p>
+            <h1>Cryptids of California</h1>
+            <div class="navbar">
+  <a class="active" href="#home">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a>
+</div>
+            <p></p>
         </div>
     );
 }
--- src/components/new-jersey.jsx
+++ src/components/new-jersey.jsx
@@ -1,10 +1,12 @@
 // NewJersey.jsx
+// the style of this page is from hompage.jsx because that takes priority
+
 import React from 'react';
 
 function NewJersey() {
     return (
         <div>
-            <h1>Welcome to New Jersey</h1>
+            <h1>Cryptids of New Jersey</h1>
             <p>This is the New Jersey page content.</p>
         </div>
     );
