99ff6869e145dbdb1a5ac5a425dcdfdbaa236cae
Anthony Littlejohn <ant05man@yahoo.com>
2024-05-27 12:10:10 -0400

    added sandy hook sea serpent to the table

--- src/components/new-jersey.jsx
+++ src/components/new-jersey.jsx
@@ -5,8 +5,9 @@ import { Link } from 'react-router-dom';
 import '../styles/new-jersey.css';
 
 const creatures = [
-    { name: 'Jersey Devil', imageUrl: '/The-Jersey-Devil.png'}
-];
+    { name: 'Jersey Devil', imageUrl: '/The-Jersey-Devil.png', location: 'Pine Barrens, NJ', description: 'Winged Chimera/Human hybrid, that reportedly has combination of different characteristics of other animals such as wings of a bat, hoofed feet, two short front legs, horse face/head, and long tail'},
+    {name: 'Sandy Hook Sea Serpent', imageUrl: '/Sandy-Hook-Sea-Serpent.jpg', location: 'Coast of Sandy Hook, in the Atlantic Ocean', description: 'Serpentine, huge, undulating creature, estimated to be around 50ft long. Horned bulldog-like face whose body tapers into sharply pointed tail.' }
+]; 
 
 function NewJersey() {
     return (
@@ -21,6 +22,8 @@ function NewJersey() {
                     <tr>
                         <th>Creature</th>
                         <th>Image</th>
+                        <th>Location</th>
+                        <th>Description</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -28,6 +31,8 @@ function NewJersey() {
                         <tr key={index}>
                             <td>{creature.name}</td>
                             <td><img src={creature.imageUrl} alt={creature.name} width="100" /></td>
+                            <td>{creature.location}</td>
+                            <td>{creature.description}</td>
                         </tr>
                     ))}
                 </tbody>
